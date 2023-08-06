9792c6f8dac3c808de20f8825c6529901ab20d9e
dpak141 <dpak141@gmail.com>
2023-08-06 08:57:16 +0545

    Chaining

--- Async/index.js
+++ Async/index.js
@@ -1,6 +1,6 @@
 // const btnEl = document.getElementById("new-deck");
+
 // function handelClick() {
-//   console.log("Clicked");
 //   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 //     .then((response) => response.json())
 //     .then((data) => console.log(data));
@@ -15,57 +15,19 @@
 
 // setTimeout(callback, 2000);
 
-// // ----------------------------------------
-
-// const people = [
-//   { name: "Deepak", hasPet: true },
-//   { name: "Finzo", hasPet: true },
-//   { name: "Suman", hasPet: false },
-//   { name: "Rakesh", hasPet: true },
-//   { name: "Navin", hasPet: false },
-//   { name: "Pinto", hasPet: false },
-// ];
-
-// function gimmeThePets(person) {
-//   return person.hasPet;
-// }
-
-// const peopleWithPets = people.filter(gimmeThePets);
-// console.log(peopleWithPets);
-
-// const peoples = [
-//   { name: "Jill", hasPet: true, age: 18 },
-//   { name: "Alson", hasPet: true, age: 17 },
-//   { name: "Bob", hasPet: false, age: 18 },
-// ];
-
-// function gimmeAdult(person) {
-//   return person.age >= 18;
-// }
-
-// const age = peoples.filter(gimmeAdult);
-// console.log(age);
-
-const people = [
-  { name: "Deepak", hasPet: true },
-  { name: "Finzo", hasPet: true },
-  { name: "Suman", hasPet: false },
-  { name: "Rakesh", hasPet: false },
-];
-
-function filterArray(array, callback) {
-  const resultingArray = [];
-  for (let item of array) {
-    const shouldBeIncluded = callback(item);
-    if (shouldBeIncluded) {
-      resultingArray.push(item);
-    }
-  }
-  return resultingArray;
-}
-
-const peopleWithPets = filterArray(people, function (person) {
-  return person.hasPet;
+document.getElementById("new-deck").addEventListener("click", function () {
+  console.log("Clicked");
 });
 
-console.log(peopleWithPets);
+const voters = [
+  { name: "Deepak", email: "deepak@gmail.com", voted: true },
+  { name: "Mahi", email: "mahi@gmail.com", voted: true },
+  { name: "Neha", email: "neha@gmail.com", voted: false },
+  { name: "Human", email: "human@gmail.com", voted: true },
+  { name: "Animal", email: "animal@gmail.com", voted: false },
+];
+
+let voterEmail = voters
+  .filter((voter) => voter.voted)
+  .map((voter) => voter.email);
+console.log(voterEmail);
